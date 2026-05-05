fd2fabf1377bee3f6af37059d5a9f6141ad119a6
samira-2002-code <samiraassemlali2@gmail.com>
2026-05-05 15:45:54 +0100

    feat: implement borrowBook logic with validation

---
+++ src/Entities/Book.php
@@ -0,0 +1,25 @@
+<?php
+
+class Book {
+    private $title;
+    private $author;
+    private $isAvailable;
+
+    public function __construct($title, $author) {
+        $this->title = $title;
+        $this->author = $author;
+        $this->isAvailable = true;
+    }
+
+    public function isAvailable() {
+        return $this->isAvailable;
+    }
+
+    public function setAvailable($value) {
+        $this->isAvailable = $value;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+}
