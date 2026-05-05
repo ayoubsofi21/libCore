4643035ddacb81746351befed92039679bd1b9a1
samira-2002-code <samiraassemlali2@gmail.com>
2026-05-05 12:39:17 +0100

    first commit

---
+++ src/Entities/Member.php
@@ -0,0 +1,13 @@
+<?php
+class Member{
+    private $name;
+    private $email;
+    private $isActive;
+    private $borrowbooks =[];
+
+    public function __construct($name,$email)
+    {
+    }
+
+}
+?>
