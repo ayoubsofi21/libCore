c1f27fa77bfd432b60974e27c9d54007cc13bbe9
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-05-06 12:47:59 +0100

    create tables (books,users,members and borrows)

---
+++ db.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE library;
+USE library;
+
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    author VARCHAR(100),
+    isbn VARCHAR(50),
+    status VARCHAR(20)
+);
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE
+);
+CREATE TABLE members (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    type ENUM('student', 'teacher'),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+CREATE TABLE borrows (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT,
+    member_id INT,
+    borrow_date DATE,
+    return_date DATE NULL,
+    status ENUM('borrowed', 'returned') DEFAULT 'borrowed',
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (member_id) REFERENCES members(id)
+);
