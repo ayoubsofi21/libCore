ff06c93eb04b9b3c1d9334800f65b6c37c0b1823
ayoubsofi21 <ayoubsofi03@gmail.com>
2026-05-07 17:15:22 +0100

    update mainAdmin

--- mainAdmin.php
+++ mainAdmin.php
@@ -7,9 +7,7 @@ require "src/Entities/Member.php";
 require "src/Services/Library.php";
 
 $library = new Library($pdo);
-
 while (true) {
-
     echo "\n===== MENU =====\n";
     echo "1. Ajouter livre\n";
     echo "2. Ajouter membre\n";
@@ -17,11 +15,8 @@ while (true) {
     echo "4. Supprimer livre\n";
     echo "5. Réparer livre\n";
     echo "0. Quitter\n";
-
     $choice = readline("Choix: ");
-
     switch ($choice) {
-
         case 1:
             $library->addBook(new Book(
                 readline("Titre: "),
@@ -30,7 +25,6 @@ while (true) {
             ));
             echo "Livre ajouté\n";
             break;
-
         case 2:
             $library->addMember(new Member(
                 readline("Nom: "),
@@ -39,24 +33,19 @@ while (true) {
             ));
             echo "Membre ajouté\n";
             break;
-
         case 3:
             $library->displayBooks();
             break;
         case 4:
-            
             $id = readline("ID à supprimer: ");
             $library->removeBook($id);
             break;
-
         case 5:
             $id = readline("ID à réparer: ");
             $library->repairBook($id);
             break;
-
         case 0:
-            exit("Au revoir 👋\n");
-
+            exit("vous ete quiter\n");
         default:
             echo "Choix invalide\n";
     }
