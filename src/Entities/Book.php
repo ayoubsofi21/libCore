1e7d5cec929e50575500bab2541004d70cdc97b0
ayoubsofi <ayoub@email.com>
2026-05-08 10:51:33 +0100

    update Book entity class

--- src/Entities/Book.php
+++ src/Entities/Book.php
@@ -1,63 +1,37 @@
-<<<<<<< HEAD
-<?php 
-    class Book{
-            private $title;
-            private $author;
-            private $isbn;
-            private  $status;
-            public function __construct($title,$author,$isbn){
-                $this->title = $title;
-                $this->author = $author;
-                $this->isbn = $isbn;
-                $this->status = "available";
-
-            }
-            public function getTitle() {
-                return $this->title;
-            }
-
-            public function getAuthor() {
-                return $this->author;
-            }
-
-            public function getIsbn() {
-                return $this->isbn;
-            }
-
-            public function getStatus() {
-                return $this->status;
-            }
-
-            public function setStatus($status) {
-                $this->status = $status;
-            }
-
-    }
-?>
-=======
 <?php
 
 class Book {
+
     private $title;
     private $author;
-    private $isAvailable;
+    private $isbn;
+    private $status;
 
-    public function __construct($title, $author) {
+    public function __construct($title, $author, $isbn) {
+
         $this->title = $title;
         $this->author = $author;
-        $this->isAvailable = true;
-    }
-
-    public function isAvailable() {
-        return $this->isAvailable;
-    }
-
-    public function setAvailable($value) {
-        $this->isAvailable = $value;
+        $this->isbn = $isbn;
+        $this->status = "available";
     }
 
     public function getTitle() {
         return $this->title;
     }
+
+    public function getAuthor() {
+        return $this->author;
+    }
+
+    public function getIsbn() {
+        return $this->isbn;
+    }
+
+    public function getStatus() {
+        return $this->status;
+    }
+
+    public function setStatus($status) {
+        $this->status = $status;
+    }
 }
->>>>>>> origin/member
